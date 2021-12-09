5fe487f91e568ed1feb6d1ee2c27e6bc4a3daeb7
Vincenzo Fasano <vfasano@hawk.iit.edu>
2021-12-09 03:44:49 -0800

    Add more quotes

--- js/main.js
+++ js/main.js
@@ -5,6 +5,11 @@
     quotes[2] = "Comedy = Tragedy + Time";
     quotes[3] = "The world is your oyster! Let's just hope you arent allergic to seafood";
     quotes[4] = "You can catch flies with honey, but you'll catch more honeys being fly";
+    quotes[5] = "You never notice glass until it breaks";
+    quotes[6] = "What's the difference between a literalist and a kleptomaniac? One takes things literally and the other takes things, literally";
+    quotes[7] = "You're incomparable, like a...";
+    quotes[8] = "You might try and fail, but what matters is you don't fail at trying";
+    quotes[9] = "The Los Angeles Angels translates to The The Angels Angels";
     var rand = Math.floor(Math.random()*quotes.length);
     document.write(quotes[rand]);
   }
